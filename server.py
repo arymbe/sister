2119c16998e3b75afe79dd6cd3612fb7b595055b
Muhammad Ibrahim Oswaldo <ibamoswaldo@gmail.com>
2014-03-11 19:44:29 +0700

    Update server.py

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ import socket
 import sys
 import pickle
 
+#ini buat setting koneksi
 host = 'localhost'
 port = 1234
 server_address = (host,port)
@@ -18,14 +19,21 @@ size = 1024
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(server_address)
 server.listen(backlog)
+
+# buka inputan file
 strPath = 'cuaca.txt'
 f=open(strPath)
 
+# baca inputan file
+# buat array yang berindex string
 dict = {}
 semua = ''
 for line in f:
+	#di split berdasarkan koma, untuk mendapatkan nama hari dan cuacanya
 	temp = line.split(',')
+	#dict[Hari] bernilai cuaca
 	dict[temp[0]] = temp[1]
+	#variabel semua untuk menyimpan semua keterangan cuaca
 	semua = semua + '\n' + temp[0] + ' : ' + temp[1]
 
 #strText=f.read()
@@ -38,11 +46,12 @@ input = [server]
 #menunggu client baru atau client yang lain untuk menginputkan masukan yang baru
 running = 1
 
+#ketika server dinyalakan
 while running :
     print "Waiting multi connection"
-
+#siap menerima inputan
     inputready, outputready, exceptready = select.select(input, [], [])
-
+#menyimpan urutan perintah dan alamat client
     for s in inputready :
         if s == server:
             #handle the server socket
@@ -56,7 +65,7 @@ while running :
             if (test == 'keluar') :
                 running = 0
                 break
-
+#fungsi untuk keluar server
         else:
             #handle all other sockets
             data = s.recv(size)
@@ -66,11 +75,13 @@ while running :
                 s.send(cobaPickle)
                 running = 0
                 break
-
+#menyimpan imputan yang di split berdasarkan spasi
             temp = data.split(' ')
             ret = ''
+            #ketika inputan bertuliskan semua dia akan mengembalikan variabel semuaa
             if temp[1] == 'semua' :
                 ret = semua
+            #jika tidak, dia akan ngeprint nama hari, dan cuaca pada hari tersebut
             else :
                 for key, value in dict.items() :
                     if (key == temp[1]) :
